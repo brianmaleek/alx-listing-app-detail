260d6347d40ceefc1ac07d9e7b9b03c736ded91f
Brian Maleek <maleekmalik@gmail.com>
2025-10-04 15:14:16 +0300

    Add common components: Button, Card, Pill, Footer, Header, Layout, BookingSection, PropertyDetail, and ReviewSection

---
+++ components/common/Button.tsx
@@ -0,0 +1,20 @@
+import { ButtonProps } from "../../interfaces/";
+import React from "react";
+
+export default function Button({
+    label,
+    onClick,
+    disabled = false,
+    type = "button",
+}: Readonly<ButtonProps>) {
+
+    return (
+        <button
+            onClick={onClick}
+            disabled={disabled}
+            type={type}
+        >
+            {label}
+        </button>
+    );
+}
